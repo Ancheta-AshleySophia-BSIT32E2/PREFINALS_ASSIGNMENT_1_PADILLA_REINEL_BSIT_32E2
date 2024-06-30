307abef6f744e15559c45f3a8ceeb062fd2dc00e
Ancheta-AshleySophia-BSIT32E2 <161100971+Ancheta-AshleySophia-BSIT32E2@users.noreply.github.com>
2024-06-30 23:46:40 +0800

    Padilla

---
+++ PREFINALSAUTH_Padilla_Reinel/ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProtectedApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            
+            var userInfo = new
+            {
+                Name = "Reinel Padilla",
+                Section = "32E2",
+                Course = "BSIT"
+            };
+
+            var funFacts = new[]
+            {
+                "I hate programming",
+                "I enjoy playing video games",
+                "I love dancing",
+            };
+
+            return Ok(new { UserInfo = userInfo, FunFacts = funFacts });
+        }
+    }
+}
